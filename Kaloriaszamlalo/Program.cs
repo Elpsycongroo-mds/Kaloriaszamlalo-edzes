39094bb84ecd99e90eeb57780d746eca32146a69
b3gyotam <b3gyotam@vasvari.org>
2025-10-21 11:53:14 +0200

    Adatbevitel

--- Kaloriaszamlalo/Program.cs
+++ Kaloriaszamlalo/Program.cs
@@ -10,6 +10,47 @@ namespace Kaloriaszamlalo
     {
         static void Main(string[] args)
         {
+            // 1. Adatbevitel
+
+            string name = "";
+            double weight = 0.0;
+            int goal = 0;
+            int trainingDays = 0;
+            int[] intensityLevels = null;
+
+            string planType = "";
+            int baseDuration = 0;
+            double calorieMultiplier = 0.0;
+
+            string input = "";
+            bool isValid = false;
+
+            Console.WriteLine("C# Projekt: Egyszerű Edzéstervező");
+            Console.WriteLine();
+
+
+            do
+            {
+                Console.Write("Kérlek add meg a teljes neved (pl. Vezetéknév Keresztnév): ");
+                name = Console.ReadLine();
+
+
+
+                string[] parts = name.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+                bool isNameFormatOk = parts.Length >= 2 && parts.All(p => !string.IsNullOrEmpty(p) && char.IsUpper(p[0]));
+
+                if (!isNameFormatOk)
+                {
+                    Console.WriteLine("HIBA: Rossz név formátum! A nevek nagy betűvel kezdődnek.");
+                    isValid = false;
+                }
+                else
+                {
+                    isValid = true;
+                }
+            } while (!isValid);
         }
     }
 }
